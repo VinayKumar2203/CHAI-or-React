774eed96630d1d5164a81ffa396bad282a9591a6
Vinay Kumar <vinaysikhera2203@gmail.com>
2024-01-28 13:37:24 +0530

    talwind css

---
+++ talwind css/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './component/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+   
+  return (
+    <>
+   <h1 className='bg-green-400 text-black p-4 
+   rounded-xl mb-4' >tailwindcss test</h1>
+    <Card userName="Vinay" btnType="click Me"/>
+    <Card userName="Neha" btnType='visit me'/>
+
+    </>
+  )
+}
+
+export default App
